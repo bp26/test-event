7f47574035a64f49f6d25ae3093ec8d7a15ef595
bp26 <aleshak260006@gmail.com>
2024-04-28 15:27:45 +0300

    commit

--- src/config.ts
+++ src/config.ts
@@ -4,8 +4,8 @@ import { SESSION_MAX_AGE, CLIENT_BASE_URL } from './constants/index.js';
 
 export const SESSION_CONFIG: SessionOptions = {
   secret: process.env.SESSION_SECRET || '',
-  saveUninitialized: true,
-  resave: true,
+  saveUninitialized: false,
+  resave: false,
   proxy: true,
   cookie: {
     maxAge: SESSION_MAX_AGE,
